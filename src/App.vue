2616b106d34f4b6ceb55e7d2232744ea88134616
Reydel Ocon <reydel321@gmail.com>
2022-01-24 14:58:00 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+<h1>sign up form</h1>
+<Signup/>
+</template>
+
+<script>
+import Signup from './components/SignUpForm.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Signup
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+body{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  background: #eee;
+}
+</style>
---
+++ src/components/SignUpForm.vue
@@ -0,0 +1,62 @@
+<template>
+  <form>
+      <label for="email">email</label>
+        <input v-model="email" type="email" required>
+      <label for="password">password</label>
+        <input type="password" v-model="password">
+      <label for="role">role</label>
+        <select name="role" id="role">
+            <option value="webdev">web dev</option>
+            <option value="webdev">web dev</option>
+            <option value="webdev">web dev</option>
+            <option value="webdev">web dev</option>
+      </select>
+      <label for="skills">skills:</label>
+        <input type="text">
+    <input type="checkbox">
+    <label for="accept">terms</label>
+  </form>
+  <h1>{{email}}</h1>
+  <h1>{{password}}</h1>
+
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            email: '',
+            password: null
+        }
+    }
+}
+</script>
+
+<style scoped>
+form {
+    max-width: 420px;
+    margin: 30px auto;
+    background: white;
+    text-align: left;
+    padding: 40px;
+    border-radius: 10px;
+  }
+label {
+    color: #aaa;
+    display: inline-block;
+    margin: 25px e 15px;
+    font-size: 0.6em;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    font-weight: bold; 
+  }
+input {
+    display: block;
+    padding: 10px 6px;
+    width: 100%;
+    box-sizing: border-box;
+    border: none;
+    border-bottom: 1px solid #ddd;
+    color: #555;
+  }
+</style>
